f72964efdcfdff648223d79a636c84a46ae864c8
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-04-06 00:24:37 +0200

    Added list od recipes to index page

--- src/main/java/rysich/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/rysich/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,34 +1,23 @@
 package rysich.springframework.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import rysich.springframework.spring5recipeapp.domain.Category;
-import rysich.springframework.spring5recipeapp.domain.UnitOfMeasure;
-import rysich.springframework.spring5recipeapp.repositories.CategoryRepository;
-import rysich.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
-
-import java.util.Optional;
+import rysich.springframework.spring5recipeapp.services.RecipeService;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categotyRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categotyRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categotyRepository = categotyRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-
-        Optional<Category> categoryOptional = categotyRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-        System.out.println("UOM Id is: " + unitOfMeasure.get().getId());
-
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 
---
+++ src/main/java/rysich/springframework/spring5recipeapp/services/RecipeService.java
@@ -0,0 +1,10 @@
+package rysich.springframework.spring5recipeapp.services;
+
+import org.springframework.stereotype.Service;
+import rysich.springframework.spring5recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+}

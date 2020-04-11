ca6244c9861e167f7760ee6abe5cbccadf6afe3a
vitaliyRysich <vitaliy.rysich@gmail.com>
2020-04-11 14:19:53 +0200

    Used Project Lombok

--- src/main/java/rysich/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/rysich/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -1,11 +1,14 @@
 package rysich.springframework.spring5recipeapp.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import rysich.springframework.spring5recipeapp.domain.Recipe;
 import rysich.springframework.spring5recipeapp.repositories.RecipeRepository;
 
 import java.util.HashSet;
 import java.util.Set;
+
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -17,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.info("I'm in the service!!!");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;

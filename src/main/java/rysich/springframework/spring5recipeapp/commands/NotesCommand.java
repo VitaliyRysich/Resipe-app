ea49b32cfac45a64900df3f9c10e0d774c635724
vitaliyRysich <vitaliy.rysich@gmail.com>
2020-04-20 22:56:33 +0200

    Add commands

---
+++ src/main/java/rysich/springframework/spring5recipeapp/commands/NotesCommand.java
@@ -0,0 +1,14 @@
+package rysich.springframework.spring5recipeapp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NotesCommand {
+    private Long id;
+    private String recipeNotes;
+
+}
